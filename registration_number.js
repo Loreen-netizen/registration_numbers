d9ba3ccd1c36f6f8a2e4fd696fe057d8c1770046
Loreen-netizen <loreenmuusha@gmail.com>
2020-07-16 15:40:40 +0200

    Dynamically adding DOM elements

--- registration_number.js
+++ registration_number.js
@@ -1,10 +1,17 @@
 var regNumberInput = document.getElementById("regNumberInput");
 var addButton = document.getElementById("addButton");
-
+var regNumberList = document.getElementById("regNumberList");
 
 var addButtonClicked = function(){
-alert("addBtnClicked")
+var newListItem = document.createElement("li");
+newListItem.classList.add("regNumberLabels");
+if (regNumberInput.value !== ""){
+    newListItem.innerHTML = regNumberInput.value;
+    regNumberList.appendChild(newListItem);
 }
+
+}
+
 
 
 addButton.addEventListener("click", addButtonClicked);
