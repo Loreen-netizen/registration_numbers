e353c3c776a14dd74797221795116398b3ae535f
Loreen-netizen <loreenmuusha@gmail.com>
2020-07-23 11:48:54 +0200

    mentor feedback fixed

--- registration_number.js
+++ registration_number.js
@@ -2,47 +2,83 @@ var regNumberInput = document.getElementById("regNumberInput");
 var addButton = document.getElementById("addButton");
 var regNumberList = document.getElementById("regNumberList");
 var allRegNumbers = [];
-var filteredTowns = [];
 var theTown = document.getElementById("townList");
+var errorLabel = document.getElementById("errorLabel");
 var filterButton = document.getElementById("filterButton");
+var clearButton = document.getElementById("clearButton");
+
+window.onload = function () {
+    if (JSON.parse(localStorage.getItem("regNumberList")) !== null)
+        allRegNumbers = JSON.parse(localStorage.getItem("regNumberList"))
+    else allRegNumbers = []
+
+};
+
+
 var addButtonClicked = function () {
-    var newListItem = document.createElement("li");
-    newListItem.classList.add("regNumberLabels");
-    if (regNumberInput.value !== "") {
-        newListItem.innerHTML = regNumberInput.value;
-        regNumberList.appendChild(newListItem);
+    if  (JSON.parse(localStorage.getItem("regNumberList")) !== null)
+        allRegNumbers = JSON.parse(localStorage.getItem("regNumberList"))
+       else{
+           allRegNumbers = []
+      } ;
 
-        allRegNumbers.push(regNumberInput.value);
-    }
-}
+    let regNumberValue = regNumberInput.value.toUpperCase();
+    let regNumberRegex = /(C[AJYL]\s\d{3}-\d{3})$|C[AJYL]\s\d{2,5}$/;
+    let result = regNumberRegex.test(regNumberValue);
+    if((result === true) && (!allRegNumbers.includes(regNumberValue)))  {
+        
+        var newListItem = document.createElement("li");
+        newListItem.classList.add("regNumberLabels");
+        
+        if (regNumberValue !== "") {
+            newListItem.innerHTML = regNumberValue;
+            regNumberList.appendChild(newListItem);
 
-var townFunction = function () {
+            allRegNumbers.push(regNumberValue);
+            localStorage.setItem("regNumberList", (JSON.stringify(allRegNumbers)));
+            errorLabel.innerHTML = "";
+           }
+      }
+    else {
+        errorLabel.innerHTML = "Please Enter Valid Reg Number"
+    };
+  }
 
-    if (theTown.value === "Paarl") {
+var townFunction = function(){
+    
+    var filteredTowns = [];
+
+    if (JSON.parse(localStorage.getItem("regNumberList")) !== null)
+        allRegNumbers = JSON.parse(localStorage.getItem("regNumberList"))
+    
+        else {
+            allRegNumbers = []
+        };
+
+    if (theTown.value === "CJ") {
+        
         for (let i = 0; i < allRegNumbers.length; i++) {
             if (allRegNumbers[i].startsWith("CJ"))
                 filteredTowns.push(allRegNumbers[i]);
         }
     }
-    if (theTown.value === "Cape Town") {
+    if (theTown.value === "CA") {
         for (let i = 0; i < allRegNumbers.length; i++) {
             if (allRegNumbers[i].startsWith("CA"))
                 filteredTowns.push(allRegNumbers[i]);
         }
     }
-    if (theTown.value === "George") {
-        for (let i = 0; i < allRegNumbers.length; i++) {
-            if (allRegNumbers[i].startsWith("CAw") || allRegNumbers[i].startsWith("CAG"))
-                filteredTowns.push(allRegNumbers[i]);
-        }
-    }
-    else if (theTown.value === "Stellenbosch") {
+    if (theTown.value === "CL") {
         for (let i = 0; i < allRegNumbers.length; i++) {
             if (allRegNumbers[i].startsWith("CL"))
                 filteredTowns.push(allRegNumbers[i]);
         }
 
     }
+    else if (theTown.value === "") {
+        filteredTowns = allRegNumbers;
+    };
+
     showTheList(filteredTowns);
 }
 
@@ -59,6 +95,24 @@ function showTheList(filteredTowns) {
     }
 }
 
+var clearLocalStorage = function(){
+    localStorage.removeItem("regNumberList")
+    regNumberList.innerHTML = "";
+}
+
+window.onload = function () {
+    if (JSON.parse(localStorage.getItem("regNumberList")) !== null) {
+        var townsArray = JSON.parse(localStorage.getItem("regNumberList"));
+        for (let i = 0; i < townsArray.length; i++) {
+
+            var newListItem = document.createElement("li");
+            newListItem.classList.add("regNumberLabels");
+            newListItem.innerHTML = townsArray[i];
+            regNumberList.appendChild(newListItem);
+        }
+    }
+};
 
 addButton.addEventListener("click", addButtonClicked);
-filterButton.addEventListener("click", townFunction);
+// filterButton.addEventListener("click", townFunction);
+clearButton.addEventListener("click",clearLocalStorage);
